b5c242bb231790b93dcce142ec4d4ecf2d64d625
arcoiris <deborlora@gmail.com>
2014-08-03 15:41:37 -0400

    Add create action with view and error display (incomplete update action)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,42 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+  before_action :safe_post, only: [:create, :update]
+  
   def index
+    @post = Post.all 
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(safe_post)
+
+    if @post.save 
+      flash[:notice] = "Your post has been saved!"
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
 
   end
+
+  private
+    def safe_post
+      params.require(:post).permit!
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
 	has_many :categories, through: :post_categories
 	has_many :comments
 	belongs_to :user
+
+	validates :title, :url, presence: :true
 end
