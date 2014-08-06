c4cae769df9bc658e9cf6f5c296a7b3760b1173e
arcoiris <deborlora@gmail.com>
2014-08-06 14:10:11 -0400

    Add comment save logic to controller, add comment partial for rendering and iteration, and return index page view back to normal after an accidental deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController 
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = Comment.new(params.require(:comment).permit(:body).merge(post_id: params[:post_id], user_id: 1))
+		# to do: remove hardcoded user id
+		if @comment.save 
+			flash[:notice] = "Your comment has been posted!"
+			redirect_to post_path(@path)
+		else
+			render 'posts/show'
+		end
+	end
 end
+
+=begin
+
+ {"utf8"=>"✓",
+ "authenticity_token"=>"gJUY0+1meG0SWDQTI6FucfLsE/BS4aRxZg2j2Dbu8ok=",
+ "comment"=>{"body"=>""},
+ "commit"=>"Post comment",
+ "action"=>"create",
+ "controller"=>"comments",
+ "post_id"=>"1"}
+
+=end
