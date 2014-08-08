cc5058b6d19f4c95c8406203c8753ce45f773bc0
arcoiris <deborlora@gmail.com>
2014-08-08 14:28:36 -0400

    Add fix url and friendly_time helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def fix_url(url)
+		url.starts_with?("http://") ? url : "http://#{url}"
+	end
+
+	def friendly_time(dt)
+		dt.localtime.strftime("%m/%d/%Y %l:%M%P %Z")
+	end
 end
