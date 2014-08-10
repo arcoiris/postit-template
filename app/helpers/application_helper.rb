17e05270bee3fbd296772b9e4ec911eb7057f142
arcoiris <deborlora@gmail.com>
2014-08-10 16:06:39 -0400

    Add small edits to helpers and view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 		url.starts_with?("http://") ? url : "http://#{url}"
 	end
 
-	def friendly_time(dt)
+	def friendly_datetime(dt)
 		dt.localtime.strftime("%m/%d/%Y %l:%M%P %Z")
 	end
 end
