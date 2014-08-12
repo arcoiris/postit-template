c33a53763553d15b798d99e5f7cfd1e1bd436c1d
arcoiris <deborlora@gmail.com>
2014-08-12 13:17:11 -0400

    Add login logic w/ not logout controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,26 @@ class SessionsController < ApplicationController
  end
 
  def create
+ 		# user.authenticate('password')
+		# 1. get user obj
+		# 2. see if the pw matches
+		# 3. if so, log in
+		# 4. if not, error message
+		user = User.find_by_username(params[:username])
+
+	 	if user && user.authenticate(params[:password])
+	 		flash[:notice] = "You have logged in!"
+	 		session[:user_id] = user.id
+	 		redirect_to root_path
+	 	else
+	 		flash[:error] = "Something went wrong with your login attempt"
+	 		render 'new'
+	 	end
  end
 
- def destroy 
+ def destroy
+ 	flash[:notice] = "You have logged out."
+ 	session[:user_id] = nil
+ 	redirect_to root_path
  end
 end
