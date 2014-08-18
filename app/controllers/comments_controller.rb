4a3dc0f47751cf363e294ba4d3e97b084343bc4a
arcoiris <deborlora@gmail.com>
2014-08-18 17:45:08 -0400

    Clean up comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController 
-	before_action :require_user, only: [:create]
-
+	before_action :require_user
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.build(params.require(:comment).permit(:body))
@@ -15,13 +14,13 @@ class CommentsController < ApplicationController
 	end
 
 	def vote
-		@comment = Comment.find(params[:id])
-		vote = Vote.create(vote: params[:vote], voteable: @comment, user: current_user)
+		comment = Comment.find(params[:id])
+		vote = Vote.create(vote: params[:vote], voteable: comment, user: current_user)
 
 		if vote.valid? 
 			flash[:notice] = 'Your vote was counted!'
 		else
-			flash[:errors] = 'You can only vote for that once'
+			flash[:errors] = 'You can only vote for that once' 
 		end
 
 		redirect_to :back
