b02a1bec9a4ec992d7122f719ee5b20e153e919c
arcoiris <deborlora@gmail.com>
2014-09-04 21:57:53 -0400

    Only allow admins and creators of posts to edit posts &heart;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?, :require_admin
+  helper_method :current_user, :logged_in?, :require_admin, :access_denied
 
   def current_user
   	@current_user ||= User.find_by(id: session[:user_id])
