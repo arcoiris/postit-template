33617fd7f38b89af657050b640e7da32a8c01f92
arcoiris <deborlora@gmail.com>
2014-09-06 12:44:44 -0400

    Add more objects to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,12 @@ Category.create(name: "Humor")
 User.create(username: "bob", password: "password", password_confirmation: "password")
 User.create(username: "hank", password: "password", password_confirmation: "password")
 
-Post.create(title: "Reddit Is Awesome!", url: "reddit.com", description: "Yeah, this is a really good funny site!", user_id: 1)
+Post.create(title: "Reddit Is Awesome!", url: "reddit.com", description: "Le Reddit, the moral backbone of a planet.", user_id: 1)
 Post.create(title: "A Great Programmer News Site", url: "hackernews.com", description: "Yeah, this is a really good news site!", user_id: 2)
+Post.create(title: "Tealeaf Academy", url: "gotealeaf.com", description: "You won't need to read tealeaves in order to build Rails apps thanks to Tealeaf Academy", user_id: 1)
 
+PostCategory.create(post_id: 1, category_id: 1)
 PostCategory.create(post_id: 1, category_id: 4)
 PostCategory.create(post_id: 2, category_id: 1)
 PostCategory.create(post_id: 2, category_id: 2)
+PostCategory.create(post_id: 3, category_id: 3)
